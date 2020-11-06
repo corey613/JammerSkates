fd11aa0cd3604a9dbdd40bcc223167024824a9a6
coreym613 <57112820+coreym613@users.noreply.github.com>
2020-11-06 15:09:45 +0000

    Initial commit

---
+++ javaScript/script.js
@@ -0,0 +1,14 @@
+
+/* Nav Burger Slide */
+
+const burger =  document.getElementById('burgerButton');
+const navUl = document.getElementById('nav');
+
+burger.addEventListener('click', ()=> {
+    navUl.classList.toggle('show');
+});
+
+/* Nav Burger Slide End */
+
+
+
